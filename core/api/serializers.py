1f5503b7bb8b24a09df4ccbb32a515a41b1ef1b9
ZIN MYO <overmidnight12@gmail.com>
2019-10-16 13:50:12 -0700

    my changes

---
+++ core/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from core.models import Item
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = (
+            ''
+        )
