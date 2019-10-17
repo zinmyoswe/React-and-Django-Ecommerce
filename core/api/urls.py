65ebd6c62bfd6ab94fff0d1a21035facf6d8b011
ZIN MYO <overmidnight12@gmail.com>
2019-10-17 12:52:43 -0700

    my changes

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ItemListView
-
+from .views import (
+    ItemListView,
+    AddToCartView
+)
 urlpatterns = [
     path('product-list/', ItemListView.as_view(), name='product-list')
+    path('add-to-cart/', AddToCartView.as_view(), name='add-to-cart')
 ]
