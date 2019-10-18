2f4e79d9cf48ed464253d58ac9e02e7528ba0447
ZIN MYO <overmidnight12@gmail.com>
2019-10-18 14:16:27 -0700

    my changes

--- src/store/actions/cart.js
+++ src/store/actions/cart.js
@@ -1,23 +1,23 @@
 import axios from "axios";
-import * as actionTypes from "./actionTypes";
+import { CART_START, CART_SUCCESS, CART_FAIL } from "./actionTypes";
 import { authAxios } from "../../utils";
 
 export const cartStart = () => {
   return {
-    type: actionTypes.AUTH_START
+    type: CART_START
   };
 };
 
 export const cartSuccess = token => {
   return {
-    type: actionTypes.AUTH_SUCCESS,
+    type: CART_SUCCESS,
     token: token
   };
 };
 
 export const cartFail = error => {
   return {
-    type: actionTypes.AUTH_FAIL,
+    type: CART_FAIL,
     error: error
   };
 };
