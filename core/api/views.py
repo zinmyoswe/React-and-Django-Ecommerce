441fd3fe33b02bba57b8e68499a9f793c7c56a9c
ZIN MYO <overmidnight12@gmail.com>
2019-10-18 19:23:45 -0700

    my changes

--- core/api/views.py
+++ core/api/views.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
